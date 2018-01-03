ce3daab695cd782587af27e546c6a5e163e1a0d6
hailong11 <hailong11@staff.weibo.com>
2018-01-03 19:29:52 +0800

    更新 WebViewController

--- KKApplication/KKApplication/KKWebViewController.h
+++ KKApplication/KKApplication/KKWebViewController.h
@@ -10,14 +10,16 @@
 #import <KKApplication/KKPageViewController.h>
 #import <WebKit/WebKit.h>
 
-@interface KKWebViewController : UIViewController<KKViewController,WKScriptMessageHandler>
+@interface KKWebViewController : UIViewController<KKViewController,WKScriptMessageHandler,WKNavigationDelegate>
 
 @property(nonatomic,strong,readonly) KKPageController * pageController;
-@property(nonatomic,strong,readonly) WKWebView * webView;
+@property(nonatomic,strong) IBOutlet WKWebView * webView;
 @property(nonatomic,strong) NSString * url;
 
 -(WKWebView *) loadWebView;
 
 -(WKWebViewConfiguration *) loadWebViewConfiguration;
+
+-(IBAction) doCloseAction:(id)sender;
 
 @end
--- KKApplication/KKApplication/KKWebViewController.m
+++ KKApplication/KKApplication/KKWebViewController.m
@@ -10,28 +10,48 @@
 #import <WebKit/WebKit.h>
 
 
-@interface KKWebViewController ()
+@interface KKWebViewController () {
+    BOOL _topbar_hidden;
+    UIColor * _topbar_backgroundColor;
+    UIColor * _topbar_tintColor;
+    UIColor * _topbar_barTintColor;
+}
 
 @end
 
 @implementation KKWebViewController
 
+@synthesize webView = _webView;
 @synthesize application = _application;
 @synthesize pageController = _pageController;
 @synthesize action = _action;
 
--(void) loadView {
-    self.view = [self loadWebView];
+-(WKWebView *) webView {
+    if(_webView == nil) {
+        _webView = [self loadWebView];
+        [_webView setNavigationDelegate:self];
+    }
+    return _webView;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    {
+        WKWebView * v = self.webView;
+        v.frame = self.view.bounds;
+        v.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        
+        [self.view addSubview:v];
+    }
+    
     NSURL * u = nil;
     if([self.url hasPrefix:@"app://"]) {
         u = [NSURL fileURLWithPath:[[self.application path] stringByAppendingPathComponent:[self.url substringFromIndex:6]] ];
     } else {
         u = [NSURL URLWithString:self.url];
     }
+
     [self.webView loadRequest:[NSURLRequest requestWithURL:u]];
 }
 
@@ -47,10 +67,6 @@
         v = [action kk_getString:@"scheme"];
     }
     self.url = v;
-}
-
--(WKWebView *) webView {
-    return (WKWebView *) self.view;
 }
 
 -(UIView *) contentView {
@@ -74,6 +90,26 @@
     
     [userContentController addUserScript:[[WKUserScript alloc] initWithSource:@"kk = { run : function(path,query) { window.webkit.messageHandlers.run.postMessage({ path : path, query: query}); } , setData:function(data) { window.webkit.messageHandlers.data.postMessage(data); }, onData:function(){} }" injectionTime:WKUserScriptInjectionTimeAtDocumentStart forMainFrameOnly:YES]];
     
+    {
+        NSString * v = [self.action kk_getString:@"beforeScript"];
+        if([v length]) {
+            NSString * code = [NSString stringWithContentsOfFile:[[self.application path] stringByAppendingPathComponent:v] encoding:NSUTF8StringEncoding error:nil ];
+            if(code) {
+                [userContentController addUserScript:[[WKUserScript alloc] initWithSource:code injectionTime:WKUserScriptInjectionTimeAtDocumentStart forMainFrameOnly:YES]];
+            }
+        }
+    }
+    
+    {
+        NSString * v = [self.action kk_getString:@"afterScript"];
+        if([v length]) {
+            NSString * code = [NSString stringWithContentsOfFile:[[self.application path] stringByAppendingPathComponent:v] encoding:NSUTF8StringEncoding error:nil ];
+            if(code) {
+                [userContentController addUserScript:[[WKUserScript alloc] initWithSource:code injectionTime:WKUserScriptInjectionTimeAtDocumentEnd forMainFrameOnly:YES]];
+            }
+        }
+    }
+    
     [userContentController addScriptMessageHandler:self name:@"run"];
     [userContentController addScriptMessageHandler:self name:@"data"];
     
@@ -85,6 +121,9 @@
 - (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message {
     
     if([message.name isEqualToString:@"run"]) {
+        
+        self.webView.opaque = NO;
+        
         [_pageController recycle];
         _pageController = [[KKPageController alloc] init];
         _pageController.application = self.application;
@@ -105,8 +144,19 @@
                 }
                 
             } keys:@[@"action",@"data"] context:nil];
-        
+            
         }
+        
+        {
+            // 关闭
+            __weak KKWebViewController * v = self;
+            [_pageController.observer on:^(id value, NSArray *changedKeys, void *context) {
+                
+                [v doCloseAction:nil];
+                
+            } keys:@[@"action",@"close"] context:nil];
+        }
+        
         [_pageController run:self];
     } else if([message.name isEqualToString:@"data"]) {
         
@@ -118,6 +168,167 @@
             }
         }
     }
+}
+
+
+-(IBAction) doCloseAction:(id)sender {
+    
+    if(self.navigationController) {
+        [self.navigationController popViewControllerAnimated:YES];
+    } else if(self.presentedViewController) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }
+    
+}
+
+
+-(void) viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self.pageController didAppear];
+}
+
+-(void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    {
+        id v = [self.pageController.observer get:@[@"page",@"topbar",@"hidden"] defaultValue:nil];
+        if(v) {
+            _topbar_hidden = [self.navigationController isNavigationBarHidden];
+            [self.navigationController setNavigationBarHidden:[v boolValue] animated:NO];
+        }
+    }
+    
+    {
+        id v = [self.pageController.observer get:@[@"page",@"topbar",@"background-color"] defaultValue:nil];
+        if(v) {
+            _topbar_backgroundColor = [self.navigationController.navigationBar backgroundColor];
+            [self.navigationController.navigationBar setBackgroundColor:[UIColor KKElementStringValue:[v kk_stringValue]]];
+        }
+    }
+    
+    {
+        id v = [self.pageController.observer get:@[@"page",@"topbar",@"tint-color"] defaultValue:nil];
+        if(v) {
+            _topbar_tintColor = [self.navigationController.navigationBar tintColor];
+            [self.navigationController.navigationBar setTintColor:[UIColor KKElementStringValue:[v kk_stringValue]]];
+        }
+    }
+    
+    {
+        id v = [self.pageController.observer get:@[@"page",@"topbar",@"bar-tint-color"] defaultValue:nil];
+        if(v) {
+            _topbar_barTintColor = [self.navigationController.navigationBar barTintColor];
+            [self.navigationController.navigationBar setBarTintColor:[UIColor KKElementStringValue:[v kk_stringValue]]];
+        }
+    }
+    
+    [self.pageController willAppear];
+}
+
+-(void) viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    
+    {
+        id v = [self.pageController.observer get:@[@"page",@"topbar",@"hidden"] defaultValue:nil];
+        if(v) {
+            [self.navigationController setNavigationBarHidden:_topbar_hidden animated:NO];
+        }
+    }
+    
+    {
+        id v = [self.pageController.observer get:@[@"page",@"topbar",@"background-color"] defaultValue:nil];
+        if(v) {
+            [self.navigationController.navigationBar setBackgroundColor:_topbar_backgroundColor];
+        }
+    }
+    
+    {
+        id v = [self.pageController.observer get:@[@"page",@"topbar",@"tint-color"] defaultValue:nil];
+        if(v) {
+            [self.navigationController.navigationBar setTintColor:_topbar_tintColor];
+        }
+    }
+    
+    {
+        id v = [self.pageController.observer get:@[@"page",@"topbar",@"bar-tint-color"] defaultValue:nil];
+        if(v) {
+            [self.navigationController.navigationBar setBarTintColor:_topbar_barTintColor];
+        }
+    }
+    
+    [self.pageController willDisappear];
+}
+
+-(void) viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+    [self.pageController didDisappear];
+}
+
+-(void) viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    [self.pageController layout:self];
+}
+
+-(void) doAction:(NSDictionary *) action {
+    
+    NSArray * keys = [[action kk_getString:@"keys"] componentsSeparatedByString:@"."];
+    
+    NSDictionary * data = [action kk_getValue:@"data"];
+    
+    NSMutableDictionary * vv = [NSMutableDictionary dictionaryWithCapacity:4];
+    
+    if([data isKindOfClass:[NSDictionary class]]) {
+        [vv addEntriesFromDictionary:data];
+    }
+    
+    vv[@"url"] = [action kk_getString:@"url"];
+    
+    NSLog(@"[KK] %@",[action kk_getString:@"url"]);
+    
+    if([keys count] > 0) {
+        
+        if(self.pageController == nil) {
+            [self.application.observer set:keys value:vv];
+        } else {
+            [self.pageController.observer set:keys value:vv];
+        }
+        
+    }
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{
+    
+    NSArray * actions = [self.action kk_getValue:@"actions"];
+    
+    NSString * url = [navigationAction.request.URL absoluteString];
+    
+    if([actions isKindOfClass:[NSArray class]]) {
+        
+        for(NSDictionary * action in actions) {
+            
+            NSString * v = [action kk_getString:@"prefix"];
+            
+            if(v && [url hasPrefix:v] ) {
+                
+                NSMutableDictionary * vv = [NSMutableDictionary dictionaryWithDictionary:action];
+                
+                vv[@"url"] = url;
+                
+                [NSObject cancelPreviousPerformRequestsWithTarget:self];
+                [self performSelector:@selector(doAction:) withObject:vv afterDelay:0.03];
+                
+                decisionHandler(WKNavigationActionPolicyCancel);
+                
+                return;
+            }
+            
+        }
+        
+    }
+
+    decisionHandler(WKNavigationActionPolicyAllow);
 }
 
 @end
